b91a69ae23e427b1a3a9a2bb502191673323111f
Mocurion <games@aljoschaboersch.de>
2025-04-11 10:23:06 +0200

    test

--- player.gd
+++ player.gd
@@ -108,5 +108,5 @@ func _on_attack_duration_timer_timeout() -> void:
 
 func _on_ready() -> void:
 	$"../HUD/Control/health_label".text = "health: " + str(player_health)
-	self.global_transform = (Transform2D(0, DisplayServer.window_get_size()/2))
+	self.global_transform = (Transform2D(0, DisplayServer.window_get_size()/4))
 	self.global_scale = Vector2(2,2)
